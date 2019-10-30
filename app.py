33932601a41947672ff2cb68bae8dd9e31d5dad7
ramanpandey <ramanpandey@bitbucket.org>
2019-10-30 13:28:14 +0530

    added api to get hospital name and minor changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 import json
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
   
 app = Flask(__name__) 
 
@@ -12,29 +12,39 @@ hospitals = [
       {"text":"Status","type":"string"}
     ],
     "rows":[
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>New York University</a>","5","No Connection"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>The University Hospital of Giessen and Marburg</a>","2","No Connection"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>Cleveland Clinic Foundation</a>","9","Error"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>Johns Hopkins University</a>","3","Error"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>OhioHealth</a>","8","Shut Down"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>University of Rochester</a>","4","OK"],
-      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1'>Washington University</a>","3","OK"]
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=nyu'>New York University</a>","5","No Connection"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=uhgm'>The University Hospital of Giessen and Marburg</a>","2","No Connection"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=ccf'>Cleveland Clinic Foundation</a>","9","Error"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=jhu'>Johns Hopkins University</a>","3","Error"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=oh'>OhioHealth</a>","8","Shut Down"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=ur'>University of Rochester</a>","4","OK"],
+      ["<a href='http://localhost:3000/d/SVYoZG0Zk/biogen-dashboard-2?orgId=1&hname=wu'>Washington University</a>","3","OK"]
     ],  
     "type":"table"
   }
 ]
-  
+
+hospitalDict = {
+  'nyu': 'New York University',
+  'uhgm': 'The University Hospital of Giessen and Marburg',
+  'ccf': 'Cleveland Clinic Foundation',
+  'jhu': 'Johns Hopkins University',
+  'oh': 'OhioHealth',
+  'ur': 'University of Rochester',
+  'wu': 'Washington University'
+}
+
 @app.route('/') 
 def hello_world(): 
     return jsonify({
   "requests_handled": random.randint(7209, 14324),
   "requests_duration_milliseconds": random.randint(1122473,1235257),
-  "request_failures": random.randint(2, 7),
+  "request_failures": int(random.randint(2, 7)),
   "documents_loaded": {
     "fast": random.randint(1,7),
     "slow": random.randint(16,60)
   },
-  "disconnected_hospitals": random.randint(2, 5),
+  "disconnected_hospitals": int(random.randint(2, 5)),
   "success_percentage": random.randint(0,100),
   "cpu_usage": random.randint(0,100),
   "memory": random.randint(0,100),
@@ -72,6 +82,15 @@ def search_data():
 @app.route('/query', methods=["POST"]) 
 def query_data(): 
     return Response(json.dumps(hospitals),  mimetype='application/json')
+
+@app.route('/hospitalName', methods=["GET"]) 
+def get_hospital_name(): 
+    hname = request.args.get('hospitalName')
+    return Response(hospitalDict[hname], status=200,  mimetype='text/plain', headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    })
   
 if __name__ == '__main__':
     app.run()  
